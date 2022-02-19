589d2807cd8a817937b8ab715da81ff31ae6c7c8
acer <thanglv@neo.vn>
2022-02-20 00:25:31 +0700

    update

---
+++ src/main/java/vn/neo/entity/Permission.java
@@ -0,0 +1,56 @@
+package vn.neo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author thanglv on 2/20/2022
+ * @project open-identity-server
+ */
+@Entity
+@Table(name = "permission")
+public class Permission implements Serializable {
+    @Id
+    private Long permissionId;
+    private String code;
+    private String displayName;
+    private Date createdDate;
+
+    public Permission() {
+    }
+
+    public Long getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
