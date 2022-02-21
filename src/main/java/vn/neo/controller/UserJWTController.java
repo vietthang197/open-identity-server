ead27e99888c45e7c68b32b7b2ff70124ba3d7df
acer <thanglv@neo.vn>
2022-02-21 18:03:47 +0700

    update

--- src/main/java/vn/neo/controller/UserJWTController.java
+++ src/main/java/vn/neo/controller/UserJWTController.java
@@ -1,5 +1,8 @@
 package vn.neo.controller;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +22,17 @@ import javax.validation.Valid;
 public class UserJWTController {
 
     private final UserInfoService userInfoService;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public UserJWTController(UserInfoService userInfoService) {
+    public UserJWTController(UserInfoService userInfoService, AuthenticationManagerBuilder authenticationManagerBuilder) {
         this.userInfoService = userInfoService;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
     @PostMapping("/authenticate")
     public BasicResponseDto login(@RequestBody @Valid LoginVM loginVM) {
+        UsernamePasswordAuthenticationToken aupt = new UsernamePasswordAuthenticationToken(loginVM.getUsername(), loginVM.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(aupt);
         return new BasicResponseDto();
     }
 }
